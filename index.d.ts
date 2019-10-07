8d1aa3c8938565dd8f0731b4527e96442ae20250
Tyler Akins <fidian@rumkin.com>
2019-10-07 12:48:06 -0500

    Updating typescript + release 1.0.6

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { default as Dizzy, DizzyProvider, BulkProvider } from 'dizzy';
 
-export default function dizzyPromisifyBluebird(dizzy: typeof Dizzy);
+export default function dizzyPromisifyBluebird(dizzy: typeof Dizzy): void;
 
 declare module 'dizzy' {
     interface DizzyProvider {
